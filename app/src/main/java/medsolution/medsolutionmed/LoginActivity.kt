7956b95dc0983454518c2050deffe6f873ba6c62
jcaiqueoliveira <j.caique_fla@hotmail.com>
2018-05-19 03:13:44 -0300

    add login screen

--- app/src/main/java/medsolution/medsolutionmed/LoginActivity.kt
+++ app/src/main/java/medsolution/medsolutionmed/LoginActivity.kt
@@ -3,11 +3,10 @@ package medsolution.medsolutionmed
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_login2)
     }
 }
---
+++ app/src/main/java/medsolution/medsolutionmed/SplashActivity.kt
@@ -0,0 +1,34 @@
+package medsolution.medsolutionmed
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.support.v7.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_splash.*
+import medsolution.gone
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            checkLogin()
+        }, 1000)
+    }
+
+    fun checkLogin() {
+
+        /* quando current user é nulo estamos deslogados */
+        val mAuth = FirebaseAuth.getInstance()
+        if (mAuth.currentUser == null) {
+            startActivity(Intent(this, LoginActivity::class.java))
+        } else {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+        progressBar.gone()
+        finish()
+    }
+}
