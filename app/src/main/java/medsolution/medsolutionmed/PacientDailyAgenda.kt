8ad99415a4dbc64dc4305170083fa4ce4c523600
Gabriel Menezes <gabrielunitone@gmail.com>
2018-05-19 09:13:11 -0300

    Lista de pacientes inicializada

---
+++ app/src/main/java/medsolution/medsolutionmed/PacientDailyAgenda.kt
@@ -0,0 +1,13 @@
+package medsolution.medsolutionmed
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PacientDailyAgenda : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pacient_daily_agenda)
+    }
+
+}
